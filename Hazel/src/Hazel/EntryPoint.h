a3290d585da144ba9f132e15cad88aeda4f063cf
xiaose715 <727955657@qq.com>
2021-12-16 02:04:45 +0800

    Added log API and macro. Added spdlog submodule.

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,12 +1,16 @@
 #pragma once
 
-#include "Application.h"
-
 #ifdef HZ_PLATFORM_WINDOWS
 
 extern Hazel::Application* Hazel::CreateApplicaton();
 
 int main(int argc, char** argv) {
+	
+	Hazel::Log::Init();
+	HZ_CORE_ERROR("Hello Jcq");
+	int a = 10;
+	HZ_INFO("a == {0}", a);
+
 	auto app = Hazel::CreateApplicaton();
 	app->Run();
 	delete app;
