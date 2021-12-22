970e442d4d296f54be123d3aaf6e23bc6c426bb2
xiaose715 <727955657@qq.com>
2021-12-23 01:38:41 +0800

    Added input polling.

--- Hazel/src/Platform/Windows/WindowsWindow.h
+++ Hazel/src/Platform/Windows/WindowsWindow.h
@@ -23,6 +23,7 @@ namespace Hazel {
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
 
+		inline virtual void* GetNativeWindow() const { return m_window; }
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
